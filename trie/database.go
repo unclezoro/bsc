479f018cdd34d64010b2d2003aca34cdb142fb32
j75689 <j75689@gmail.com>
2022-05-05 15:31:11 +0800

    fix deadlock

--- trie/database.go
+++ trie/database.go
@@ -734,7 +734,6 @@ func (db *Database) Commit(node common.Hash, report bool, callback func(common.H
 		batch.Reset()
 	}
 	// Move the trie itself into the batch, flushing if enough data is accumulated
-	db.lock.RLock()
 	nodes, storage := len(db.dirties), db.dirtiesSize
 	db.lock.RUnlock()
 
